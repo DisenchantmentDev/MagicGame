6d7b360f9c187c4e50857573b514b2e567012279
Lance_Baron <lbaron082@gmail.com>
2026-02-21 15:45:52 -0700

    Added Resizeable Window Flag in setup

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char **argv) {
     InitWindow(600, 600, "Game");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(30);
     while (!WindowShouldClose()) {
         BeginDrawing();
