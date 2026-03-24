66c8e08e0a1cf7ee369db8ebf57e99d671d60900
jpustinger2 <jack.pustinger@colostate.edu>
2026-03-24 11:07:50 -0600

    Added Basic PlayerStats

--- src/player.c
+++ src/player.c
@@ -7,6 +7,9 @@ void init_player(Player *p, Image sheet) {
         texture; // This will need to change later to a DrawTexturePro() call
     p->pos = (Vector2){PLAYER_STARTING_X, PLAYER_STARTING_Y};
     p->mov_dir = (Vector2){MOV_RIGHT, MOV_UP};
+    p->health = MAX_HP;
+    p->shield = MAX_SHIELD;
+    p->armor = 0;
 }
 
 void draw_current_glumbus(Player *p) {
@@ -39,4 +42,33 @@ void player_move(Player *p) {
         p->pos.y -= 2;
         p->mov_dir.y = MOV_UP;
     }
+}
+
+void take_damage(Player *p, int incoming) {
+    if(p->shield != 0) {
+        if(p->shield < incoming) p->shield = 0; //Shield will block all damage to health when it is broken, no punch through
+        else p->shield -= incoming;
+    }
+    else {
+        int trueDamage = incoming * (1.0 - (p->armor / 100)); //This mitigates damage to health based on armor ie 60 armor = 60% damage reduction
+        if(p->health < trueDamage){
+            p->health = 0;
+            death(p);
+        }
+        else p->health -= trueDamage;
+    }
+}
+
+void heal(Player *p, int incoming) {
+    if(incoming + p->health > MAX_HP) p->health = MAX_HP;
+    else p->health += incoming;
+}
+
+void armor_increase(Player *p, int incoming) {
+    if(incoming + p->armor > MAX_ARMOR) p->armor = MAX_ARMOR;
+    else p->armor += incoming;
+}
+
+void death(Player *p) {
+    //Empty for now, placeholder to do something on player death
 }
